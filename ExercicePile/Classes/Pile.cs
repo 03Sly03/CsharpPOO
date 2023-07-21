fb89183eee06516eb7043282091d8891b3251c66
03Sly03 <slydevwebproject@outlook.fr>
2023-07-21 09:55:00 +0200

    ExercicePile

---
+++ ExercicePile/Classes/Pile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicePile.Classes
+{
+    internal class Pile<T>
+    {
+        private List<T> _elements = new List<T>();
+
+        public List<T> Elements { get => _elements; set => _elements = value; }
+
+        public void Empiler(T valeurAEmpiler)
+        {
+            Elements.Add(valeurAEmpiler);
+            Console.WriteLine("\n------------------- Empile -------------------");
+            Console.WriteLine($"L'élément \"{valeurAEmpiler}\" a été ajouté à la pile !\nLa Pile compte maintenant {Elements.Count} élément(s)");
+        }
+
+        public void Depiler()
+        {
+            Console.WriteLine("\n------------------- Dépile -------------------");
+            Console.Write($"L'élément {Elements.ElementAt(Elements.Count - 1)} ");
+            Elements.RemoveAt(Elements.Count - 1);
+            Console.WriteLine($" a été dépilé\nLa Pile compte {Elements.Count} élément(s)");
+        }
+    }
+}
---
+++ ExercicePile/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ExercicePile.Classes;
+
+Pile<string> maPile = new Pile<string>();
+
+maPile.Empiler("test");
+maPile.Empiler("test2");
+maPile.Empiler("test3");
+maPile.Empiler("test4");
+maPile.Empiler("test5");
+maPile.Depiler();
+
+Pile<int> maPile2 = new Pile<int>();
+
+maPile2.Empiler(1);
+maPile2.Empiler(2);
+maPile2.Empiler(3);
+maPile2.Empiler(4);
+maPile2.Empiler(5);
+maPile2.Depiler();
